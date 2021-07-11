e0b1522e40dd959dd97595ebc2eb342517889ec0
tylanm <tylanmcpherson@gmail.com>
2021-07-11 14:27:08 -0700

    Algorithm planned in comments

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 --Initial Commit
 
-This project will implement the classic game of tic-tac-toe within the CLI.
+This project will implement the classic game of rock-paper-scissors within the CLI.
 
 Looking to futher expand this CLI program into one that has a GUI in the future.
---
+++ scripts/rps.js
@@ -0,0 +1,13 @@
+console.log("Welcome to the console, let us play some rock-paper-scissors!");
+
+//Initial algorithm
+
+//Init round count variable and start new round
+//Ask player if they choose rock, paper or scissors
+//Store user input in variable
+//Allow the computer to randomly pick one of the choices and store result
+//compare user vs cpu choice and determine outcome
+//Declare outcome and print scoreboard
+//Begin another around
+
+//repeat... 
